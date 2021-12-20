74ada92f3636068e2e71d079aea2c30b969635cd
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-20 20:45:31 +0530

    Basic styling added

--- Calculator-App/src/App.js
+++ Calculator-App/src/App.js
@@ -1,7 +1,32 @@
+import classes from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <div className={classes.calculator}>
+        <div className={classes.output}>
+          <div className={classes.previousOperand}></div>
+          <div className={classes.currentOperand}></div>
+        </div>
+        <button className={classes.spanTwo}>AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className={classes.spanTwo}>=</button>
+      </div>
     </div>
   );
 }
