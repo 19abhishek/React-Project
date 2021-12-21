01731ce34ebb6142c48e7e90c57ec268c960c4ac
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-21 16:06:06 +0530

    Calculator App completed

--- Calculator-App/src/App.js
+++ Calculator-App/src/App.js
@@ -112,7 +112,7 @@ function evaluate({ currentOperand, previousOperand, operation }) {
     case "*":
       computation = prev * current;
       break;
-    case "÷":
+    case "/":
       computation = prev / current;
       break;
   }
