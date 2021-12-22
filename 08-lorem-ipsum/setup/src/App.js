22fdd4e1be9bc1cfbcd8a34d79585aa9f6de5413
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-22 23:49:06 +0530

    Lorem Ipsum starter

---
+++ 08-lorem-ipsum/setup/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  console.log(data.length);
+  return <h2>lorem ipsum project setup</h2>;
+}
+
+export default App;
