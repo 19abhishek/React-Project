1d4be0c39d4c933a4889dca1388b1e20250ff9fc
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-22 22:44:42 +0530

    Basic styling done

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaChevronLeft, FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+import "./App.css";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [value, setValue] = useState(0);
+
+  const { id, image, name, title, quote } = data[value];
+  return (
+    <div>
+      <header className="header">Reviews</header>
+      <div className="container">
+        <button className="btn">
+          <FiChevronLeft />
+        </button>
+        <span className="info">
+          <img className="img" src={image} alt="Person" />
+          <div className="name">{name}</div>
+          <div className="title">{title}</div>
+          <div className="quote">{quote}</div>
+        </span>
+        <button className="btn">
+          <FiChevronRight />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
