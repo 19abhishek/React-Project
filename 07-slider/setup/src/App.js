180e7d51403341f1a1da3b6a795a78731675ca33
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-22 23:15:23 +0530

    Slider complete

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -6,12 +6,41 @@ import "./App.css";
 function App() {
   const [value, setValue] = useState(0);
 
+  const leftClickHandler = () => {
+    if (value === 0) {
+      setValue(data.length - 1);
+    } else {
+      const x = value - 1;
+      setValue(x);
+    }
+  };
+  const rightClickHandler = () => {
+    if (value === data.length - 1) {
+      setValue(0);
+    } else {
+      const x = value + 1;
+      setValue(x);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (value === data.length - 1) {
+        setValue(0);
+      } else {
+        const x = value + 1;
+        setValue(x);
+      }
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [value]);
+
   const { id, image, name, title, quote } = data[value];
   return (
     <div>
       <header className="header">Reviews</header>
       <div className="container">
-        <button className="btn">
+        <button className="btn" onClick={leftClickHandler}>
           <FiChevronLeft />
         </button>
         <span className="info">
@@ -20,7 +49,7 @@ function App() {
           <div className="title">{title}</div>
           <div className="quote">{quote}</div>
         </span>
-        <button className="btn">
+        <button className="btn" onClick={rightClickHandler}>
           <FiChevronRight />
         </button>
       </div>
