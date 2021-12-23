24b3aa9c4b7541a3a3377227f2c32c2340044f78
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-23 09:39:39 +0530

    Lorem Ipsum completed

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,8 +1,48 @@
 import React, { useState } from "react";
-import data from "./data";
+import info from "./data";
+import classes from "./App.module.css";
 function App() {
-  console.log(data.length);
-  return <h2>lorem ipsum project setup</h2>;
+  const [value, setValue] = useState(0);
+  const [data, setData] = useState([]);
+
+  const changeHandler = (e) => {
+    setValue(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setData(info.slice(0, value));
+  };
+  // console.log(data);
+  return (
+    <div>
+      <header className={classes.header}>TIRED OF BORING LOREM IPSUM?</header>
+      <form className={classes.form} onSubmit={formSubmitHandler}>
+        <label>Paragraph:</label>
+        <input
+          className={classes.input}
+          type="number"
+          min="1"
+          max="8"
+          step="1"
+          onChange={changeHandler}
+          value={value}
+        />
+        <button type="submit" className={classes.btn}>
+          Generate
+        </button>
+      </form>
+      <div className={classes.info}>
+        {data.map((para, idx) => {
+          return (
+            <div key={idx}>
+              <p>{para}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
