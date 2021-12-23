e07692182492fce45e52f4e392dd8ecbd324ca26
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-23 17:01:33 +0530

    Grocery Bud stylign done

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+import Card from "./components/Card";
+import classes from "./App.module.css";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [items, setItems] = useState(["Egg", "Bread"]);
+  const [hasItems, setHasItems] = useState(items.length > 0);
+  const message = "xajnsjnkajsn";
+  return (
+    <Card className={classes.card}>
+      {/* <div className={classes.error}>{message}</div> */}
+      <header className={classes.header}>Grocery Bud</header>
+      <form className={classes.form}>
+        <input className={classes.input} placeholder="e.g eggs" type="text" />
+        <button className={classes.btn}>Submit</button>
+      </form>
+      <div>
+        {items.map((item) => {
+          return (
+            <div className={classes.item}>
+              <span>{item}</span>
+              <div>
+                <button>
+                  <FaEdit />
+                </button>
+                <button>
+                  <FaTrash />
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={classes.clear}>
+        {hasItems && <button className={classes.clearBtn}>Clear Items</button>}
+      </div>
+    </Card>
+  );
 }
 
-export default App
+export default App;
